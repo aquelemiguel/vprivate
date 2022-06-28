49389e5a8edbd062836490f3b6f6cf6651d46e06
aquelemiguel <aquelemiguel@gmail.com>
2022-06-28 17:58:37 +0100

    completely revamp service component

--- components/Service.svelte
+++ components/Service.svelte
@@ -2,7 +2,7 @@
     export let data;
 </script>
 
-<main id="{data.id}">
+<!-- <main id="{data.id}">
     <div class="container">
         <div class="service-title">
             <h1>{data.title}</h1>
@@ -26,93 +26,101 @@
             <button class="lets-talk-button" on:click={() => window.location.href = '/contactos'}>Vamos conversar?</button>
         </div>
     </div>
-</main>
+</main> -->
+
+<div class="card-container" id="{data.id}">
+    <div class="card-preview">
+        <h1 class="card-header">{data.title}</h1>
+        <div class="card-image"></div>
+        <!-- <img class="card-image" src="../../images/landing2.jpg" alt="Card" /> -->
+    </div>
+
+    <div class="card-info">
+        <div class="card-description">
+            {#each data.description as line}
+                <p>{line}</p>
+            {/each}
+        </div>
+
+        <div class="card-chips">
+            {#each data.features as feature}
+                <div class="card-chip">{ feature }</div>
+            {/each}
+        </div>
+    </div>
+</div>
 
 <style lang="scss">
-    main {
-        min-height: 100vh;
-        padding-top: 2.5rem;
+    .card-container {
+        display: flex;
+        max-width: 100%;
+        margin: 2rem;
+        overflow: hidden;
+        width: 65%;
+        border-radius: 10px;
+        box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2);
     }
 
-    .lets-talk-button {
-        font-size: 1.2em;
-        font-weight: bold;
+    .card-preview {
+        background-color: #c89e28;
+        // background-blend-mode: multiply;
+        // background-image: url(../../images/landing2.jpg);
+        color: white;
+        // padding: 2rem;
+        flex: 0 0 40%;
+        overflow: hidden;
     }
 
-    .container {
+    .card-image {
+        width: 100%;
+        height: 100%;
+        background-image: url(../../images/landing2.jpg);
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-color: #c89e28;
+        background-blend-mode: multiply;
+    }
+
+    .card-header {
+        position: absolute;
+        width: 20rem;
+        padding: 1.5rem;
+        font-size: 38px;
+    }
+
+    .card-info {
         display: flex;
         flex-direction: column;
         align-items: center;
-        justify-content: center;
-        max-width: 55%;
-        margin: 0 auto;
-        min-height: 100vh;
+        padding: 2rem;
     }
 
-    p {
-        font-size: 20px;
+    .card-description > p {
+        font-size: 18px;
     }
 
-    p:first-child {
-        margin-top: 0;
-    }
-
-    .service-title {
-        align-self: flex-start;
-        font-size: 32px;
-    }
-
-    .subservice-flex {
+    .card-chips {
         display: flex;
+        flex-wrap: wrap;
         width: 100%;
-        justify-content: space-between;
-        margin: 3rem 0 2rem 0;
+        margin-top: 0.5rem;
+        justify-content: flex-start;
     }
 
-    .subservice-item {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		text-align: center;
-		width: 240px;
-		height: 60px;
-		padding: 0 1em;
-		margin: 0.5em;
-        box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
-        border-radius: 5px;
-        padding: 10px;
-        font-weight: bold;
-        background-color: #c89e28;
+    .card-chip {
+        border: 1px solid #c89e28;
+        border-radius: 1rem;
+        margin: 0.25rem 0;
+        padding: 0.5rem;
+        font-size: 12px;
+        background: #c89e28;
         color: white;
+        cursor: pointer;
+        font-weight: bold;
     }
 
-    .service-grid {
-        display: grid;
-        grid-auto-rows: auto 5rem;
-        gap: 2rem;
-        margin: 2rem 0;
-    }
-
-    .service-image {
-        grid-column: 1;
-        grid-row: 1/3;
-
-        img {
-            max-width: 100%;
-            border-radius: 1.5em;
-            box-shadow: rgba(0, 0, 0, 0.75) 0px 20px 30px -10px;
-        }
-    }
-
-    .service-description {
-        grid-column: 2;
-        grid-row: 1;
-    }
-
-    .service-button {
-        margin-left: 2rem;
-        grid-column: 2;
-        grid-row: 2;
+    .card-chip:not(:last-of-type) {
+        margin-right: 0.5rem;
     }
 </style>
